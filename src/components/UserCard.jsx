647d43ba8218bdc2fecaee582cdd555e5aa349f4
Jamshed Alam <jamshedalam1777@gmail.com>
2025-03-26 13:05:11 +0530

    edit api make

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
 const UserCard = ({ user }) => {
-  
+
+
   const { firstName, lastName, photoUrl, age, gender, about } = user;
+
   return (
-  <div className="card bg-base-300 w-96 shadow-sm">
-   <figure>
-    <img
-      src={user.photoUrl}
-      alt="photos" />
-    </figure>
-    <div className="card-body">
-      <h2 className="card-title">{firstName + ' ' + lastName}</h2>
-        <p>A card component has a figure, a body part, and inside body there are title and actions parts
-          
-      </p>
-     <div className="card-actions justify-center my-4">
+    <div className="card bg-base-300 w-96 shadow-sm">
+      <figure>
+        <img src={photoUrl} alt="Profile" className="w-full h-48 object-cover" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + ' ' + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p>{about}</p>
+
+        <div className="card-actions justify-center my-4">
           <button className="btn btn-primary">Ignore</button>
-          <button className="btn btn-secondary">Intersted</button>
+          <button className="btn btn-secondary">Interested</button>
+        </div>
+      </div>
     </div>
-   </div>
- </div>
-  )
-}
+  );
+};
+
 
 export default UserCard;
